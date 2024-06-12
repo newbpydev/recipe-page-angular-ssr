af9b0be5dd660af2e8e27f98074887776a489811
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-06-12 10:17:45 -0300

    chore: Add necessary files and styles for recipe page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RecipePageComponent } from './pages/recipe-page/recipe-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: RecipePageComponent,
+  },
+];
